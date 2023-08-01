02abdfed4e955a0ad3b72a53a2ffdfecd4f8b400
brianlu18 <brianlu18@g.ucla.edu>
2023-07-31 19:14:48 -0500

    fixed render error

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-// import 'bootstrap/dist/css/boostrap.min.css'
+import "bootstrap/dist/css/boostrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import './index.css'
 
 // pages for router to use
